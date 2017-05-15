762f0499c2668bdf7739a1dae8b150a410f824bf
joko <joko.pramono@btpn.com>
2017-05-15 15:22:11 +0700

    test3

--- src/java/eu/sig/training/ch04/CheckingAccount.java
+++ src/java/eu/sig/training/ch04/CheckingAccount.java
@@ -13,29 +13,13 @@ public class CheckingAccount {
             throw new BusinessException("Limit exceeded!");
         }
         // 2. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0;
-        for (int i = 0; i < counterAccount.length(); i++) {
-            char character = counterAccount.charAt(i);
-            int characterValue = Character.getNumericValue(character);
-            sum = sum + (9 - i) * characterValue;
-        }
-        if (sum % 11 == 0) {
-            // 3. Look up counter account and make transfer object:
-            CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
-            Transfer result = new Transfer(this, acct, amount);
-            return result;
-        } else {
-            throw new BusinessException("Invalid account number!");
-        }
+       SavingsAccount sa =new SavingsAccount();
+        return sa.makeTransfer(counterAccount,amount);
     }
 
     public void addInterest() {
-        Money interest = balance.multiply(INTEREST_PERCENTAGE);
-        if (interest.greaterThan(0)) {
-            balance.add(interest);
-        } else {
-            balance.substract(interest);
-        }
+        SavingsAccount sa =new SavingsAccount();
+        sa.addInterest();
     }
 }
 // end::CheckingAccount[]
