24cc775b45e31faeb60fb83c40a1248148c44a72
joko <joko.pramono@btpn.com>
2017-05-15 17:10:01 +0700

    get seter

--- src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
+++ src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
@@ -2,20 +2,31 @@ package eu.sig.training.ch05.buildandsendmail;
 
 public class BuildAndSendMail {
     // tag::buildAndSendMail[]
-    public void buildAndSendMail(MailMan m, String firstName, String lastName,
-        String division, String subject, MailFont font, String message1,
-        String message2, String message3) {
+    private String mId;
+    private MailMessage mMessage;
+    public void buildAndSendMail(MailMan m,  String subject) {
         // Format the email address
-        String mId = firstName.charAt(0) + "." + lastName.substring(0, 7) + "@"
-            + division.substring(0, 5) + ".compa.ny";
+
         // Format the message given the content type and raw message
-        MailMessage mMessage = formatMessage(font,
-            message1 + message2 + message3);
         // Send message
-        m.send(mId, subject, mMessage);
+        m.send(getMid(), subject, mMessage);
     }
     // end::buildAndSendMail[]
+    public void SetMid(String firstName,String lastName,String division){
+         this.mId = firstName.charAt(0) + "." + lastName.substring(0, 7) + "@"
+                + division.substring(0, 5) + ".compa.ny";
+    }
+    public String getMid(){
+        return this.mId;
+    }
+    public void SetMessage(MailFont font, String message1, String message2, String message3){
+        this.mMessage = formatMessage(font, message1 + message2 + message3);
 
+    }
+    public MailMessage getMessage(){
+        return this.mMessage;
+    }
+//    public void SetmId()
     @SuppressWarnings("unused")
     private MailMessage formatMessage(MailFont font, String string) {
         return null;
